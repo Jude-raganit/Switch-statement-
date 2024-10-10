98df0c646ef608bdc323984f355f07e5c4de6c51
Jude-raganit <juderaganit56@gmail.com>
2024-10-10 22:07:26 +0800

    Create Task calendar.js

---
+++ Task calendar.js
@@ -0,0 +1,11 @@
+let day = "monday";
+switch ("day"){
+  case "monday";
+    console.log("your task for monday is finish your assignment ");
+    break;
+  case "Tuesday";
+    console.log("your task for Tuesday is your code");
+    break;
+    default;
+    console.log("maybe you need to do other activity");
+}
